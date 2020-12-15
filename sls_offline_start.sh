75a2a9685b1d46b5e6f99408046de02db0e78aef
Suyog Mirgal <suyog.mirgal@arrkgroup.com>
2020-12-16 02:35:39 +0530

    sls start script updated

--- sls_offline_start.sh
+++ sls_offline_start.sh
@@ -6,7 +6,7 @@ docker run --name alpine-sqs -p 9324:9324 -p 9325:9325 -d roribio16/alpine-sqs:l
 sleep 5
 echo 'local SQS started'
 
-curl http://localhost:9321/queue/default
+curl http://localhost:9324/queue/default
 
 echo ''
 echo ''
